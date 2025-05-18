74aabbc2c4ce899ab17a73db5c2398747cff6dcc
7BatStrokes <daviseven84@gmail.com>
2025-05-17 20:30:32 -0500

    style(Initial): :beers: Models for the future

--- internal/model/model.go
+++ internal/model/model.go
@@ -1,23 +1,19 @@
 package model
 
 import (
-	"github.com/google/uuid"
 	"github.com/tigerbeetle/tigerbeetle-go/pkg/types"
 )
 
-type Account struct {
+type AccountFinal struct {
 	ID        types.Uint128 // Foreign Key ID from SQL
-	Ledger    uint8         // Must be 1 for now
-	Flags     uint16        // DebitsMustNotExceedCredits, History
-	Timestamp uint64        // Unix timestamp
+	Balances  types.AccountBalance
+	Timestamp uint64 // Unix timestamp
 }
 
-type Transfer struct {
-	ID              types.Uint128 // TigerBeetle time-based ID
-	DebitAccountID  uuid.UUID     // Foreign Key ID from SQL
-	CreditAccountID uuid.UUID     // Foreign Key ID from SQL
-	Amount          types.Uint128 // Amount in smallest unit
-	Ledger          uint8         // Must be 1 for now
-	Code            uint8         // Seems like a code of all possible transfers in app
-	Timestamp       uint64        // Unix timestamp
+type TransferFinal struct {
+	ID                    types.Uint128 // TigerBeetle time-based ID
+	DebitAccountUsername  string
+	CreditAccountUsername string
+	Amount                types.Uint128 // Amount in smallest unit
+	Timestamp             uint64        // Unix timestamp
 }
